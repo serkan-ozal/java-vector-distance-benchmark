0c37e63a43a52382d06ed349d3a74b2425f49602
Serkan ÖZAL <serkanozal86@gmail.com>
2023-10-13 18:37:20 +0300

    Increase fork to `3`

--- src/test/java/net/sozal/benchmark/vector/distance/VectorDistanceCalculatorBenchmark1.java
+++ src/test/java/net/sozal/benchmark/vector/distance/VectorDistanceCalculatorBenchmark1.java
@@ -23,7 +23,7 @@ import java.util.concurrent.TimeUnit;
  * @author sozal
  */
 @BenchmarkMode(Mode.Throughput)
-@Fork(value = 1)
+@Fork(value = 3)
 @Warmup(time = 5, iterations = 3, timeUnit = TimeUnit.SECONDS)
 @Measurement(time = 30, iterations = 5, timeUnit = TimeUnit.SECONDS)
 public class VectorDistanceCalculatorBenchmark1 {
